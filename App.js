9471b9ff69e62a0122331a097deb70a25babf4b1
Thiago <thiagodsantos316@gmail.com>
2023-04-18 20:20:25 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,9 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import TodoApp from './components/Task ';
+import React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './src/screens/HomeScreen';
+import TodoApp from './src/components/Task ';
 
-export default function App() {
+const Stack = createStackNavigator();
+
+function App() {
   return (
-    <TodoApp/>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="TodoApp" component={TodoApp} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
+export default App;
